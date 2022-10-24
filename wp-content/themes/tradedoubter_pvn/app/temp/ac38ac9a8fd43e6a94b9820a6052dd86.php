dfb70e8e8905ce9f562df5585ceba62242708b23
Dominik Lewczyński <dominiklew200060@gmail.com>
2022-10-24 15:27:30 +0200

    Update wordpress to 6.0.3 version

---
+++ wp-content/themes/tradedoubter_pvn/app/temp/ac38ac9a8fd43e6a94b9820a6052dd86.php
@@ -0,0 +1,140 @@
+<?php
+return array (
+  0 => 
+  array (
+    'WCD\\AppController' => 
+    array (
+      'file' => '/home/zhornik/domains/bumerang.vot.pl/public_html/wp-dominik/wp-content/themes/tradedoubter_pvn/app/AppController/AppController.php',
+      'time' => 1636024710,
+    ),
+    'WCD\\GutenbergBlocks' => 
+    array (
+      'file' => '/home/zhornik/domains/bumerang.vot.pl/public_html/wp-dominik/wp-content/themes/tradedoubter_pvn/app/GutenbergBlocks/GutenbergBlocks.php',
+      'time' => 1636024707,
+    ),
+    'WCD\\PostTypes' => 
+    array (
+      'file' => '/home/zhornik/domains/bumerang.vot.pl/public_html/wp-dominik/wp-content/themes/tradedoubter_pvn/app/PostTypes/PostTypes.php',
+      'time' => 1636024704,
+    ),
+    'WCD\\RestAPI\\Media' => 
+    array (
+      'file' => '/home/zhornik/domains/bumerang.vot.pl/public_html/wp-dominik/wp-content/themes/tradedoubter_pvn/app/RestApi/Media/Media.php',
+      'time' => 1636024693,
+    ),
+    'WCD\\RestAPI' => 
+    array (
+      'file' => '/home/zhornik/domains/bumerang.vot.pl/public_html/wp-dominik/wp-content/themes/tradedoubter_pvn/app/RestApi/RestApi.php',
+      'time' => 1634049978,
+    ),
+    'WCD\\RestAPIItem' => 
+    array (
+      'file' => '/home/zhornik/domains/bumerang.vot.pl/public_html/wp-dominik/wp-content/themes/tradedoubter_pvn/app/RestApi/Item/Item.php',
+      'time' => 1636024690,
+    ),
+    'WCD\\RestAPI\\Sandbox' => 
+    array (
+      'file' => '/home/zhornik/domains/bumerang.vot.pl/public_html/wp-dominik/wp-content/themes/tradedoubter_pvn/app/RestApi/sandbox/Sandbox.php',
+      'time' => 1636024700,
+    ),
+    'WCD\\RestAPI\\Posts' => 
+    array (
+      'file' => '/home/zhornik/domains/bumerang.vot.pl/public_html/wp-dominik/wp-content/themes/tradedoubter_pvn/app/RestApi/Posts/Posts.php',
+      'time' => 1636024698,
+    ),
+    'WCD\\WPSiteConfig' => 
+    array (
+      'file' => '/home/zhornik/domains/bumerang.vot.pl/public_html/wp-dominik/wp-content/themes/tradedoubter_pvn/app/SiteConfig/SiteConfig.php',
+      'time' => 1663850447,
+    ),
+    'WCD\\User' => 
+    array (
+      'file' => '/home/zhornik/domains/bumerang.vot.pl/public_html/wp-dominik/wp-content/themes/tradedoubter_pvn/app/User/User.php',
+      'time' => 1636024681,
+    ),
+    'WCD\\Acf' => 
+    array (
+      'file' => '/home/zhornik/domains/bumerang.vot.pl/public_html/wp-dominik/wp-content/themes/tradedoubter_pvn/app/acf/Acf.php',
+      'time' => 1634049978,
+    ),
+    'WCD\\AcfService' => 
+    array (
+      'file' => '/home/zhornik/domains/bumerang.vot.pl/public_html/wp-dominik/wp-content/themes/tradedoubter_pvn/app/acf/AcfService.php',
+      'time' => 1634049978,
+    ),
+  ),
+  1 => 
+  array (
+    'TimberArchives' => 213,
+    'WP_CLI_Command' => 414,
+    'Timber\\Integrations\\Timber_WP_CLI_Command' => 207,
+    'WP_Site_Health' => 204,
+    'acf_third_party' => 198,
+    'WooCommerce' => 39,
+    'Tabify_Edit_Screen' => 195,
+    'Post_Type_Switcher' => 192,
+    'Dark_Mode' => 189,
+    'acf_field_text' => 186,
+    'acf_field_textarea' => 183,
+    'acf_field_number' => 180,
+    'acf_field_range' => 177,
+    'acf_field_email' => 174,
+    'acf_field_url' => 171,
+    'acf_field_password' => 168,
+    'acf_field_image' => 165,
+    'acf_field_file' => 162,
+    'acf_field_wysiwyg' => 159,
+    'acf_field_oembed' => 156,
+    'acf_field_select' => 153,
+    'acf_field_checkbox' => 150,
+    'acf_field_radio' => 147,
+    'acf_field_button_group' => 144,
+    'acf_field_true_false' => 141,
+    'acf_field_link' => 138,
+    'acf_field_post_object' => 135,
+    'acf_field_page_link' => 132,
+    'acf_field_relationship' => 129,
+    'acf_field_taxonomy' => 126,
+    'ACF_Field_User' => 123,
+    'acf_field_google_map' => 120,
+    'acf_field_date_picker' => 117,
+    'acf_field_date_and_time_picker' => 114,
+    'acf_field_time_picker' => 111,
+    'acf_field_color_picker' => 108,
+    'acf_field_message' => 105,
+    'acf_field__accordion' => 102,
+    'acf_field_tab' => 99,
+    'acf_field__group' => 96,
+    'acf_field_repeater' => 93,
+    'acf_field_flexible_content' => 90,
+    'acf_field_gallery' => 87,
+    'acf_field_clone' => 84,
+    'ACF_Location_Post_Type' => 81,
+    'ACF_Location_Post_Template' => 78,
+    'ACF_Location_Post_Status' => 75,
+    'ACF_Location_Post_Format' => 72,
+    'ACF_Location_Post_Category' => 69,
+    'ACF_Location_Post_Taxonomy' => 66,
+    'ACF_Location_Post' => 63,
+    'ACF_Location_Page_Template' => 60,
+    'ACF_Location_Page_Type' => 57,
+    'ACF_Location_Page_Parent' => 54,
+    'ACF_Location_Page' => 51,
+    'ACF_Location_Current_User' => 48,
+    'ACF_Location_Current_User_Role' => 45,
+    'ACF_Location_User_Form' => 42,
+    'ACF_Location_User_Role' => 39,
+    'ACF_Location_Taxonomy' => 36,
+    'ACF_Location_Attachment' => 33,
+    'ACF_Location_Comment' => 30,
+    'ACF_Location_Widget' => 27,
+    'ACF_Location_Nav_Menu' => 24,
+    'ACF_Location_Nav_Menu_Item' => 21,
+    'ACF_Location_Block' => 18,
+    'ACF_Location_Options_Page' => 15,
+    'CoAuthors_Plus' => 12,
+    'WP_Filesystem_direct' => 3,
+    'ReallySimpleCaptcha' => 3,
+    'WP_Automatic_Updater' => 3,
+  ),
+);
