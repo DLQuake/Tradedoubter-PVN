9de1853bd16d84c6ad3106a4289879d8e43140f8
Dominik Lewczyński <dominiklew200060@gmail.com>
2022-10-27 14:32:33 +0200

    Update project

--- wp-content/themes/tradedoubter_pvn/src/js/app.js
+++ wp-content/themes/tradedoubter_pvn/src/js/app.js
@@ -2,25 +2,17 @@
 import $ from "jquery";
 import "../sass/app.sass";
 
-import HeroHomepage from "./blocks/HeroHomepage.block";
-import Features from "./blocks/Features.block";
 import Benefits from "./blocks/Benefits.block";
+import Features from "./blocks/Features.block";
 import References from "./blocks/References.block";
-import News from "./blocks/News.block ";
 
 import BurgerMenu from "./modules/BurgerMenu.module";
-import ScrollTop from "./modules/ScrollTop.module";
 import HeaderPrimary from "./modules/HeaderPrimary.module";
-import CarouselProducts from "./modules/CarouselProducts.module";
 
 $(document).ready(() => {
-  CarouselProducts.init();
-  HeroHomepage.init();
-  BurgerMenu.init();
-  ScrollTop.init();
-  HeaderPrimary.init();
-  Features.init();
   Benefits.init();
+  Features.init();
   References.init();
-  News.init();
+  BurgerMenu.init();
+  HeaderPrimary.init();
 })
